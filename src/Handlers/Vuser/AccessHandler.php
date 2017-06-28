782ea1ecf57071feddb793ae83cd4bf66cd1b38f
vin <506512796@qq.com>
2017-06-28 17:32:03 +0800

    enhance

---
+++ src/Handlers/Vuser/AccessHandler.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vin
+ * Date: 17-6-28
+ * Time: 下午5:07
+ */
+
+namespace Notadd\Vuser\Handlers\Vuser;
+
+use Exception;
+use Illuminate\Container\Container;
+use Notadd\Foundation\Routing\Abstracts\Handler;
+use League\OAuth2\Server\AuthorizationServer;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Illuminate\Auth\AuthManager;
+use Zend\Diactoros\Response as Psr7Response;
+
+class AccessHandler extends Handler {
+
+    protected $auth;
+    protected $server;
+    protected $client_id;
+    protected $client_secret;
+
+    public function __construct(Container $container, AuthorizationServer $server, AuthManager $auth) {
+        parent::__construct($container);
+        $this -> auth = $auth;
+        $this -> server = $server;
+    }
+
+    protected function execute() {
+        if ($this -> auth ->guard('api')->user()) {
+            try {
+                $this->request->offsetSet('grant_type', 'client_credentials');
+                $this->request->offsetSet('client_id', $this->client_id);
+                $this->request->offsetSet('client_secret', $this->client_secret);
+                $this->request->offsetSet('scope', '*');
+                $request = (new DiactorosFactory)->createRequest($this->request);
+                $back = $this->server->respondToAccessTokenRequest($request, new Psr7Response());
+                $back = json_decode((string)$back->getBody(), true);
+                if (isset($back['access_token'])) {
+                    return $this -> withCode(200)
+                        -> withData($back)
+                        -> withMessage('vuser::login.1003')
+                        -> withExtra(['vcode' => 1003]);
+                }
+            } catch (Exception $exception) {
+
+                return $this -> withCode(503)
+                    -> withData([
+                        'exception_code' => $exception->getCode(),
+                        'message' => $exception->getMessage(),
+                        'trace' => $exception->getTraceAsString()
+                    ])
+                    -> withError('vuser::login.1004')
+                    -> withExtra(['vcode' => 1004]);
+            }
+        }
+
+        return $this -> withCode(200)
+            -> withMessage('vuser::login.1005')
+            -> withExtra(['vcode' => 1005]);
+    }
+
+}
